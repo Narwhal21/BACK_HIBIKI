b94cf85863a7d1dbf13b8c1c20af5afbae4a4f01
Reda <a27338@svalero.com>
2025-02-24 16:29:53 +0100

    B

--- obj/Debug/net8.0/BACK_HIBIKI.AssemblyInfo.cs
+++ obj/Debug/net8.0/BACK_HIBIKI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACK_HIBIKI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d62d6d2b463e4b6c8ada30725e32346bdf74a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d9afddb0356340a826d702f047e45f7ab9063af")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACK_HIBIKI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACK_HIBIKI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
