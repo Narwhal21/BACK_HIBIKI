737ea3054feb9984f72630ea08446cf872f4f022
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-03-04 20:01:27 +0100

    B

--- obj/Debug/net8.0/BACK_HIBIKI.AssemblyInfo.cs
+++ obj/Debug/net8.0/BACK_HIBIKI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACK_HIBIKI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36db09696fed0b12e793d0fc4a127b59978ecde9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed0b9869b8693cb96fff01f1ccabb380f9d561b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACK_HIBIKI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACK_HIBIKI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
